f50c6ee771467fd8b6228ce910a8be0b4388305a
mariazhy <na3arenko.maria@gmail.com>
2020-01-13 01:18:39 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace HomeTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter a word:");
+            string word = Console.ReadLine();
+            char[] array = word.ToCharArray();
+            Array.Reverse(array);
+            string wordReversed = new String(array);
+            if (word == wordReversed)
+            {
+                Console.WriteLine("Your word is palindrome");
+            }
+            else Console.WriteLine("Your word is not palindrome");
+        }
+    }
+}
+
