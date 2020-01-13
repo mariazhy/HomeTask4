cf747a7852b10c0f06f158ae20db91b8a171d7d0
mariazhy <na3arenko.maria@gmail.com>
2020-01-13 22:55:02 +0200

    second commit: add class PalindromeValidation and implented cicle

---
+++ PalindromeValidation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeTask4
+{
+    class PalindromeValidation
+    {
+       public bool CheckPalindrome(string wordValidated)
+        {
+            int Length = wordValidated.Length;
+            for (int i = 0; i < Length / 2; i++)
+            {
+                if (wordValidated[i] != wordValidated[Length - i - 1])
+                {
+                    return false;
+                }
+            } 
+            return true;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,11 +8,9 @@ namespace HomeTask4
         static void Main(string[] args)
         {
             Console.WriteLine("Please, enter a word:");
-            string word = Console.ReadLine();
-            char[] array = word.ToCharArray();
-            Array.Reverse(array);
-            string wordReversed = new String(array);
-            if (word == wordReversed)
+            string newWord = Console.ReadLine();
+            PalindromeValidation word = new PalindromeValidation();
+            if (word.CheckPalindrome(newWord))
             {
                 Console.WriteLine("Your word is palindrome");
             }
